ea211db9237e2c304d25fafd11e23618fe1da2c8
allan <allanmsales@gmail.com>
2024-03-09 13:02:39 -0300

    feature: New NER string in question_dict

--- entity/chatbot.py
+++ entity/chatbot.py
@@ -11,45 +11,56 @@ import json
 class Chatbot:
     def __init__(self):
         self.system = "You are a hotel host chatbot. Your answers are short and restricted to the exact informantion that you need to know."
+        self.ner = "You are a text extractor program. Your answer will be stored in a database, so you need to answer only the exact word that is asked."
         self.question_dict = {
             'name': {
                 'prompt': f'{self.system} The client just started the conversation. Elaborate a question to know the name of your interlocutor',
+                'ner': f'{self.ner} Extract the personal name in the input',
                 'value': None
                 },
             'checkin': {
                 'prompt': f'{self.system} Elaborate a short question to know the date of checkin',
+                'ner': f'{self.ner} Extract the date in the input and transform in the yyyy-mm-dd format',
                 'value': None
                 },
             'checkout': {
                 'prompt': f'{self.system} Elaborate a short question to know the day of checkout',
+                'ner': f'{self.ner} Extract the date in the input and transform in the yyyy-mm-dd format',
                 'value': None
                 },
             'guesses': {
                 'prompt': f'{self.system} Elaborate a short question to know the number of guesses',
+                'ner': f'{self.ner} Extract the number of guesses indicated by the input',
                 'value': None
             },
             'room': {
                 'prompt': f'{self.system} Elaborate a short question to know the room size. The available sizes are BIG, SMALL and MEDIUM',
+                'ner': f'{self.ner} Extract the room size indicated by the input',
                 'value': None
             },
             'payment': {
                 'prompt': f'{self.system} Elaborate a short question to know what payment method the client prefer, the available options are CREDIT CARD or DEPOSIT',
+                'ner': f'{self.ner} Extract the payment method indicated by the input',
                 'value': None 
             },
             'breakfast': {
                 'prompt': f'{self.system} Elaborate a short question to know what will be the breakfast plan, the availabel options are NO BREAKFAST, FULL BREAKFAST',
+                'ner': f'{self.ner} Extract the breakfast plan indicated by the input',
                 'value': None
             },
             'restrictions': {
                 'prompt': f'{self.system} Elaborate a short question to know if the client have some dietary restrictions',
+                'ner': f'{self.ner} Extract the dietary restictions indicated by the input',
                 'value': None
             },
             'referral': {
                 'prompt': f'{self.system} Elaborate a short question to know how did the customer find out about the hotel',
+                'ner': f'{self.ner} Extract the referral indicated by the input',
                 'value': None
             },
             'driver': {
                 'prompt': f'{self.system} Elaborate a short question to know whether the customer will need a driver or transfer service',
+                'ner': f'{self.ner} Extract YES or NO from the information if the client needs driver or transfor from what was indicated by the input',
                 'value': None
             },
             'finish': {
@@ -75,9 +86,9 @@ class Chatbot:
             ("user", "{input}")
         ])
 
-    def switch_to_ner(self, field):
+    def switch_to_ner(self, key):
         self.prompt = ChatPromptTemplate.from_messages([
-            ("system", f"You are a Named Entity Recognozer system. Answer only the {field} in the input."),
+            ("system", self.question_dict[key]['ner']),
             ("user", "{input}")
         ])
 
